54b119f4691c633a421c62eab9b3615abef8ffeb
Daniel Yee <danjamesyee@gmail.com>
2020-08-29 09:50:48 -0700

    Creates edit note form

---
+++ src/components/edit_note_form.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+class EditNoteForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.note;
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(type) {
+    return (e) => this.setState({ [type]: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+		this.props.updateNote(this.state);
+		
+  }
+
+  render() {
+    // debugger;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Title:</label>
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.handleInput("title")}
+          />
+
+          <label>Description:</label>
+          <textarea
+            value={this.state.description}
+            onChange={this.handleInput("description")}
+          ></textarea>
+
+          <button>Update</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EditNoteForm;
