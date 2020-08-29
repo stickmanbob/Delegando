22f3b33c15953c6631bf91f0d5c065c079f7ff8e
Daniel Yee <danjamesyee@gmail.com>
2020-08-29 09:50:32 -0700

    Adds update note action

--- src/actions/note_actions.js
+++ src/actions/note_actions.js
@@ -1,5 +1,6 @@
 export const CREATE_NOTE = "CREATE_NOTE";
 export const REMOVE_NOTE = "REMOVE_NOTE";
+export const UPDATE_NOTE = "UPDATE_NOTE";
 
 export const createNote = (note) => ({
   type: CREATE_NOTE,
@@ -10,3 +11,15 @@ export const removeNote = (id) => ({
   type: REMOVE_NOTE,
   id: id,
 });
+
+export const updateNote = (note) => {
+  return {
+    type: UPDATE_NOTE,
+    note: {
+      id: note.id,
+      title: note.title,
+      description: note.description,
+      colId: note.colId,
+    },
+  };
+};
