2aee0e150ba8dae46ec31493b1df8c747779bf40
Ajay Rajamani <ajayrajamani@gmail.com>
2020-08-29 12:26:52 -0700

    Refactor save button into component

---
+++ src/components/save_button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {connect} from 'react-redux';
+
+class SaveButton extends React.Component{
+
+	constructor(props) {
+		super(props);
+
+		//Bindings
+		this.save = this.save.bind(this);
+	}
+
+	save(e) {
+		e.preventDefault();
+		let storage = window.localStorage;
+		let data = JSON.stringify(this.props.state);
+		storage.setItem("data", data);
+	}
+
+
+	render(){
+		return(
+			<button onClick={this.save}>Save Boards</button>
+		)
+	}
+}
+
+
+function mSTP(state) {
+	return {
+		state: state
+	}
+}
+
+export default connect(mSTP,null)(SaveButton);
