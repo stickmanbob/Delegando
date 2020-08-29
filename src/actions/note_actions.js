18929f6959a4144c5421ef024c1462c11a9f043e
Daniel Yee <danjamesyee@gmail.com>
2020-08-29 09:01:30 -0700

    Fixes note actions to delete by id

--- src/actions/note_actions.js
+++ src/actions/note_actions.js
@@ -1,13 +1,12 @@
 export const CREATE_NOTE = "CREATE_NOTE";
 export const REMOVE_NOTE = "REMOVE_NOTE";
 
-
 export const createNote = (note) => ({
   type: CREATE_NOTE,
-  note: note
+  note: note,
 });
 
 export const removeNote = (id) => ({
   type: REMOVE_NOTE,
-  id:id
+  id: id,
 });
