1697bca7e0f2a87b6357efda4b9904be107d94a5
Daniel Yee <danjamesyee@gmail.com>
2020-08-29 09:02:51 -0700

    Fixes reducers to create note with id

--- src/reducers/columns_reducer.js
+++ src/reducers/columns_reducer.js
@@ -1,30 +1,33 @@
-import { CREATE_COLUMN, UPDATE_COLUMN, REMOVE_COLUMN } from "../actions/column_actions"
+import {
+  CREATE_COLUMN,
+  UPDATE_COLUMN,
+  REMOVE_COLUMN,
+} from "../actions/column_actions";
 
-export default function columnsReducer(state={},action){
+export default function columnsReducer(state = {}, action) {
+  Object.freeze(state);
 
-    Object.freeze(state);
-    
-    
-    switch(action.type){
-        case CREATE_COLUMN:
-            // First, let's get the next availible object id
-						let id = state.nextId;
-						
-						// create our new column object
-						let column = Object.assign({},action.column,{id:id});
+  switch (action.type) {
+    case CREATE_COLUMN:
+      // First, let's get the next availible object id
+      let id = state.nextId;
 
-            //Duplicate the current state and add our new column object
-            return Object.assign(
-                {}, 
-                state, 
-                
-                {[id]: column}, // Our new object, namespaced under its new id
-                
-								{nextId:id+1}) //Increment the next availible id by 1
-				
-				case UPDATE_COLUMN:
-						return Object.assign({},state,{[action.column.id]: action.column})
-        default:
-            return state; 
-    }
+      // create our new column object
+      let column = Object.assign({}, action.column, { id: id });
+
+      //Duplicate the current state and add our new column object
+      return Object.assign(
+        {},
+        state,
+
+        { [id]: column }, // Our new object, namespaced under its new id
+
+        { nextId: id + 1 }
+      ); //Increment the next availible id by 1
+
+    case UPDATE_COLUMN:
+      return Object.assign({}, state, { [action.column.id]: action.column });
+    default:
+      return state;
+  }
 }
