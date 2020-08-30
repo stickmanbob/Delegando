0ede6b4de68889d742aa4a295eb42c87facdbb14
Ajay Rajamani <ajayrajamani@gmail.com>
2020-08-30 16:47:34 -0700

    Updates

---
+++ precache-manifest.8d285b09fd0f3780dd7ef00af0a1ea91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604c05066b11fdb2a59314a0926c1a9b",
+    "url": "/JS-Olympics/index.html"
+  },
+  {
+    "revision": "649718f20add55627c97",
+    "url": "/JS-Olympics/static/css/main.bf32fc0a.chunk.css"
+  },
+  {
+    "revision": "b14939039a629c7a3114",
+    "url": "/JS-Olympics/static/js/2.1c39563d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JS-Olympics/static/js/2.1c39563d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649718f20add55627c97",
+    "url": "/JS-Olympics/static/js/main.86c71350.chunk.js"
+  },
+  {
+    "revision": "844a758d721e54e47a8a",
+    "url": "/JS-Olympics/static/js/runtime-main.92372b77.js"
+  },
+  {
+    "revision": "f6152c1d280faaee836c496361b88c6d",
+    "url": "/JS-Olympics/static/media/delegando.f6152c1d.gif"
+  }
+]);
